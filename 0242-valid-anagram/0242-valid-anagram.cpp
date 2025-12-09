17d617bf6b1f22d53f97bffb40604d952749c717
Salma Hafez <72113890+SalmaAhmedHafez@users.noreply.github.com>
2025-12-09 09:34:14 +0200

    Time: 4 ms (37.26%), Space: 9.8 MB (47.34%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map <char, int> mp;
+        for(auto c : s) mp[c] ++;
+        for(auto c : t) mp[c] --;
+
+        for(auto i : mp){
+            if(i.second != 0) return false;
+        }
+        return true;
+    }
+};
