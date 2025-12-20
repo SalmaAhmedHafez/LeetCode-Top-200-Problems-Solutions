ea6fc4d244dfa07a1d3e2583d5c3ee36f6a62ad4
Salma Hafez <72113890+SalmaAhmedHafez@users.noreply.github.com>
2025-12-20 08:30:50 +0200

    Time: 0 ms (100%), Space: 71.7 MB (80.81%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sm = 0, mx_sm = -1e9, mn_pre = 0;
+        int sm = 0, mx_sm = -2e4, mn_pre = 0;
         for(auto num : nums){
             sm += num;
             mx_sm = max({num, mx_sm, sm - mn_pre});
