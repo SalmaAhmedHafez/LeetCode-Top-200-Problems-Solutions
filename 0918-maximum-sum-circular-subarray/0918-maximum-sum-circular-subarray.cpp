d1f75d5728533c56231d7a8aea4cf41de12b2592
Salma Hafez <72113890+SalmaAhmedHafez@users.noreply.github.com>
2025-12-22 08:07:01 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& nums) {
-        int sm = 0, mx = -1e9, mn_pre = 0, n = nums.size();
+        int sm = 0, mx_sm = -1e9, mn_pre = 0, mx_pre = nums[0], mn_sm = 1e9;
         for(auto num : nums){
             sm += num;
-            mx = max({mx, num, sm - mn_pre});
+            mx_sm = max({mx_sm, num, sm - mn_pre});
+            mn_sm = min({mn_sm, num, sm - mx_pre});
             mn_pre = min(mn_pre, sm);
+            mx_pre = max(mx_pre, sm);
         }
-        vector <int> mx_pre(n);
-        int pre = 0;
-        for(int i = 0; i < n; i++){
-            pre += nums[i];
-            mx_pre[i] = pre;
-            if(i) mx_pre[i] = max(mx_pre[i-1], mx_pre[i]);  
-        }
-        pre = 0;
-        for(int i = n-1; i > 0; i--){
-            pre += nums[i];
-            mx = max(mx, pre + mx_pre[i-1]);
-        }
-
-        return mx;
+        return max(mx_sm, sm - mn_sm);
     }
 };
