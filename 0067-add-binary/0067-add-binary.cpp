141bd946744846dca000f7cd1b7c8783d1e80ddd
Salma Hafez <72113890+SalmaAhmedHafez@users.noreply.github.com>
2026-04-23 20:36:48 +0200

    Time: 0 ms (100%), Space: 9 MB (60.79%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int carry = 0;
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+        string ans;
+        int n = a.size(), m = b.size();
+        for(int i = 0; i < max(n, m); i++){
+            int digit = (i < n ? a[i] - '0' : 0) + (i < m? b[i]-'0' : 0) + carry;
+            if(digit >= 2) carry = 1;
+            else carry = 0;
+            ans.push_back((char)( (digit%2) + '0'));
+        }
+        if(carry) ans.push_back('1');
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
