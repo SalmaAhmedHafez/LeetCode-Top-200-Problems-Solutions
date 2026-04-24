9e3d4ece55e559fca79fe8798d7e2ca2b8c3f3e6
Salma Hafez <72113890+SalmaAhmedHafez@users.noreply.github.com>
2026-04-24 20:52:08 +0300

    Time: 2 ms (56.82%), Space: 14.4 MB (36.04%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
-    vector<int> curr;
-    vector<vector<int>> ans;
+    vector <int> curr;
+    vector <vector <int>> ans;
 
-    void rec(int idx, int sm, vector<int>& candidates, int target) {
-        if (sm == target) {
+    void rec(int idx, int sm, vector <int> &candidates, int target){
+        if(sm == target){
             ans.push_back(curr);
             return;
         }
-
-        if (idx >= candidates.size() || sm > target) return;
-
+        if(idx >= candidates.size() || sm > target) return;
+        sm += candidates[idx];
         curr.push_back(candidates[idx]);
-        rec(idx, sm + candidates[idx], candidates, target);
-        
+        rec(idx, sm, candidates, target);
+
+        sm -= candidates[idx];
         curr.pop_back();
-        rec(idx + 1, sm, candidates, target);
+        rec(idx+1, sm, candidates, target);
+    
     }
+
 
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
